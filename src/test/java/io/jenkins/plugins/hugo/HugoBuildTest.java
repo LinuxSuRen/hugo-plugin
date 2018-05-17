2f39445e8a5f37e4ae8794136deaa4a17907da20
suren <zxjlwt@126.com>
2018-05-17 19:05:44 +0800

    add test case

--- src/test/java/io/jenkins/plugins/hugo/HugoBuildTest.java
+++ src/test/java/io/jenkins/plugins/hugo/HugoBuildTest.java
@@ -1,8 +1,53 @@
 package io.jenkins.plugins.hugo;
 
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
+import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.plugins.git.GitSCM;
+import hudson.plugins.git.extensions.GitSCMExtension;
+import hudson.plugins.git.extensions.impl.SubmoduleOption;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
 /**
  * @author suren
  */
 public class HugoBuildTest
 {
+    private String repo = "https://gitee.com/arch2surenpi/hugo-plugin-test";
+
+    @Rule
+    public JenkinsRule rule = new JenkinsRule();
+
+    @Test
+    public void build() throws Exception
+    {
+        FreeStyleProject project = rule.createFreeStyleProject();
+
+        HugoBuilder builder = new HugoBuilder("");
+
+        GitSCM scm = new GitSCM(repo);
+
+        GitSCMExtension item = new SubmoduleOption(false,
+                true, true,
+                null, 10, true);
+        scm.getExtensions().add(item);
+        project.setScm(scm);
+
+        StandardUsernameCredentials credentials = new UsernamePasswordCredentialsImpl(CredentialsScope.SYSTEM, null,
+                null, "", "");
+        SystemCredentialsProvider.getInstance().getCredentials().add(credentials);
+
+        builder.setCredentialsId(credentials.getId());
+        project.getBuildersList().add(builder);
+
+        rule.createOnlineSlave();
+
+        FreeStyleBuild build = rule.buildAndAssertSuccess(project);
+        rule.assertLogContains("hugo", build);
+    }
 }
